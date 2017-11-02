6981513948c7dfc9f8ebf9434cdb6c87a20ec4a8
Jishahath <jishahath@gmail.com>
2017-11-02 12:49:21 +0530

    updates on transactions page

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -8,35 +8,35 @@
                 <div class="panel-heading">Transactions</div>
 
                 <div class="panel-body">
-                	@if (count($transactions) > 1)
+                	@if (count($transactions) > 0)
 	                    <table class="table table-striped">
-						  <thead class="thead-light">
-						    <tr> 
-						      <th scope="col">#</th>
-						      <th scope="col">Date Time</th>
-						      <th scope="col">Amount</th>
-						      <th scope="col">Transaction Type</th>
-						    </tr>
-						  </thead>
-						  <tbody>
-						  	@foreach ($transactions as $transaction)
-							    <tr>
-							      <th scope="row">{{ $loop->iteration }}</th>
-							      <td>{{ $transaction->created_at }}</td>
-							      <td>{{ $transaction->amount }}</td>
-							      <td>
-							      	{{ $transaction->type }}
-							        @if($transaction->transfer_id)
-							      	  by Transfer
-							        @endif
-							      </td>
-							    </tr>
-						    @endforeach
-						  </tbody>
-						</table>
-					@else
-						No transactions found !
-					@endif 
+												<thead class="thead-light">
+													<tr> 
+														<th scope="col">#</th>
+														<th scope="col">Date Time</th>
+														<th scope="col">Amount</th>
+														<th scope="col">Transaction Type</th>
+													</tr>
+												</thead>
+												<tbody>
+													@foreach ($transactions as $transaction)
+														<tr>
+															<th scope="row">{{ $loop->iteration }}</th>
+															<td>{{ $transaction->created_at }}</td>
+															<td>{{ $transaction->amount }}</td>
+															<td>
+																{{ $transaction->type }}
+																@if($transaction->transfer_id)
+																	by Transfer
+																@endif
+															</td>
+														</tr>
+													@endforeach
+												</tbody>
+											</table>
+									@else
+										No transactions found !
+									@endif 
                 </div>
             </div>
         </div>
